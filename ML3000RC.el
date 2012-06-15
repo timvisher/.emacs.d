bcf14f1077622aaf66d611e2a2db107352d91acc
Tim Visher <tim.visher@siemens.com>
2012-06-15 10:56:12 -0400

    Adding nonProxyHosts to gradle opts on windows. EOM

--- ML3000RC.el
+++ ML3000RC.el
@@ -1,3 +1,3 @@
 (eval-after-load "deft" '(setq deft-directory (concat (getenv "HOME") "/Dropbox/deft notes/")))
 
-(setenv "GRADLE_OPTS" "-Dhttp.proxyHost=165.226.204.104 -Dhttp.proxyPort=8080 -Xmx1500m -XX:MaxPermSize=1500m")
+(setenv "GRADLE_OPTS" "-Dhttp.proxyHost=165.226.204.104 -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=ml1002pc -Dorg.gradle.daemon=false -Xmx1500m -XX:MaxPermSize=1500m")
