06971d57d03a594b2104c89849c1a1d85b601c95
Tim Visher <tim.visher@siemens.com>
2012-06-21 15:48:17 -0400

    Correcting configuration of ML3000RC. EOM

--- ML3000RC.el
+++ ML3000RC.el
@@ -1,5 +1,5 @@
-(eval-after-load "deft" '(setq deft-directory (concat (getenv "HOME") "/Dropbox/deft notes/")))
-(eval-after-load "markdown" '(setq markdown-command "perl d:/view_store/z002w5en/bin/markdown"))
-(eval-after-load "locate" '(setq locate-command "es"))
+(eval-after-load 'deft '(setq deft-directory (concat (getenv "HOME") "/Dropbox/deft notes/")))
+(eval-after-load 'markdown-mode '(setq markdown-command "perl d:/view_store/z002w5en/bin/markdown"))
+(eval-after-load 'locate '(setq locate-command "es"))
 
 (setenv "GRADLE_OPTS" "-Dhttp.proxyHost=165.226.204.104 -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=ml1002pc -Dorg.gradle.daemon=false -Xmx1500m -XX:MaxPermSize=1500m")
