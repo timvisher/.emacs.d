33b136166eb4e2af4799393494e2806d0dbc7bef
Tim Visher <tim.visher@gmail.com>
2012-07-06 07:21:41 -0400

    Turn on column-number-mode. EOM

--- init.el
+++ init.el
@@ -281,6 +281,7 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
  '(backup-directory-alist (\` (("." \, (concat (getenv "HOME") "/.emacs.d/" system-name "-backups")))))
+ '(column-number-mode t)
  '(css-indent-offset 3)
  '(custom-enabled-themes (quote (solarized-light)))
  '(custom-safe-themes (quote ("91f2c4c623100a649cde613e8336eaa2ee144104" "62b81fe9b7d13eef0539d6a0f5c0c37170c9e248" "5600dc0bb4a2b72a613175da54edb4ad770105aa" "0174d99a8f1fdc506fa54403317072982656f127" default)))
