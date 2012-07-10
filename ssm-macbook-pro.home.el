cd624c9c45de7f401868c5d260fd1c567b063a64
Tim Visher <tim.visher@siemens.com>
2012-07-10 10:41:09 -0400

    `tim.el` doesn't seem like the proper place to keep system specific customizations. EOM

--- ssm-macbook-pro.home.el
+++ ssm-macbook-pro.home.el
@@ -1,1 +1,74 @@
+;;; ----------------------------------------------------------------------------
+;;; Let's set up the some system variables
+;;; ----------------------------------------------------------------------------
+
+(setq exec-path (split-string (getenv "PATH") ":"))
+(setq exec-path (append (list (concat (getenv "HOME") "/.gem/ruby/1.8/bin"))
+                        (let ((brew-home "/usr/local"))
+                          (list (concat brew-home "/bin")
+                                (concat brew-home "/sbin")))
+                        exec-path))
+
+(dolist (variable '("PATH" "EMACSPATH"))
+  (setenv variable "")
+  (dolist (value exec-path)
+    (setenv variable
+            (concat (getenv variable) value ":")))
+  (setenv variable (substring (getenv variable) 0 -1)))
+
+(setq exec-path (append (list (concat (getenv "HOME") "/.lein/bin")
+                              (concat (getenv "HOME") "/bin")
+                              (let ((brew-gnu-prefix (shell-command-to-string "brew --prefix coreutils")))
+                                (concat (substring brew-gnu-prefix 0 (1- (length brew-gnu-prefix))) "/libexec/gnubin")))
+                        exec-path))
+
+(dolist (variable '("PATH" "EMACSPATH"))
+  (setenv variable "")
+  (dolist (value exec-path)
+    (setenv variable
+            (concat (getenv variable) value ":")))
+  (setenv variable (substring (getenv variable) 0 -1)))
+
+(setenv "JAVA_HOME" "/Library/Java/Home")
+
+;;; ----------------------------------------------------------------------------
+;;; Let's get our modes set up
+;;; ----------------------------------------------------------------------------
+
+;;; Growl from Emacs? Boom!
+(require 'todochiku)
+
+;;; Why, oh why does brew insist on installing gnu tools at `g` prefixes?
+(eval-after-load 'grep
+  '(setq grep-find-template "gfind . <X> -type f <F> -exec grep <C> -nH <R> {} \\;"))
+
+;;; System specific deft-directory location.
 (eval-after-load "deft" '(setq deft-directory (concat (getenv "HOME") "/Dropbox/deft notes/")))
+
+;;; All the cool kids swap `⛫` and `M`
+(if (boundp 'mac-command-modifier) (setq mac-command-modifier 'meta))
+(if (boundp 'mac-option-modifier) (setq mac-option-modifier 'super))
+
+;;; ----------------------------------------------------------------------------
+;;; Set up our custom functions
+;;; ----------------------------------------------------------------------------
+
+(defun cradle-run (args)
+  (interactive "MRun Cradle with args: ")
+  (let ((current-directory default-directory)
+        (cradle-home (locate-dominating-file default-directory "build.clj")))
+    (progn
+      (cd cradle-home)
+      (shell-command (concat "cradle " args))
+      (cd current-directory))))
+
+(defun cradle-deploy ()
+  (interactive)
+  (cradle-run "deploy"))
+
+;;; ----------------------------------------------------------------------------
+;;; Start up default processes
+;;; ----------------------------------------------------------------------------
+
+;;; Always run ERC on my mac.
+(erc :server "irc.freenode.net" :nick "timvisher")
