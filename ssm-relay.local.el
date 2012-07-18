9d516c6a97da13ed4cc3a85cae3b9a3bde47551f
Tim Visher <tvisher@ssm-relay.local>
2012-07-18 11:55:25 -0400

    More changes for the workbox. EOM

--- ssm-relay.local.el
+++ ssm-relay.local.el
@@ -2,6 +2,8 @@
 ;;; Start up default processes
 ;;; ----------------------------------------------------------------------------
 
+(setq erc-nick "ttimvisher")
+
 ;;; Always run ERC on my mac.
-(erc :server "irc.freenode.net" :nick "ttimvisher")
+(erc :server "irc.freenode.net" :nick erc-nick)
 
