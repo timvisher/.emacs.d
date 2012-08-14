bb8f17a9b0b02c417668ce87bbef4142041457a9
Tim Visher <tvisher@relaynetwork.com>
2012-08-13 20:05:52 -0400

    Set up some system variables for ssm-relay. EOM

--- ssm-relay.local.el
+++ ssm-relay.local.el
@@ -1,4 +1,35 @@
 ;;; ----------------------------------------------------------------------------
+;;; Let's set up the some system variables
+;;; ----------------------------------------------------------------------------
+
+(setq exec-path (split-string (getenv "PATH") ":"))
+(setq exec-path (append (list (concat (getenv "HOME") "/.gem/ruby/1.8/bin"))
+                        (let ((brew-home "/usr/local"))
+                          (list (concat brew-home "/bin")
+                                (concat brew-home "/sbin")))
+                        exec-path))
+
+(dolist (variable '("PATH" "EMACSPATH"))
+  (setenv variable "")
+  (dolist (value exec-path)
+    (setenv variable
+            (concat (getenv variable) value ":")))
+  (setenv variable (substring (getenv variable) 0 -1)))
+
+(setq exec-path (append (list (concat (getenv "HOME") "/.lein/bin")
+                              (concat (getenv "HOME") "/bin")
+                              (let ((brew-gnu-prefix (shell-command-to-string "brew --prefix coreutils")))
+                                (concat (substring brew-gnu-prefix 0 (1- (length brew-gnu-prefix))) "/libexec/gnubin")))
+                        exec-path))
+
+(dolist (variable '("PATH" "EMACSPATH"))
+  (setenv variable "")
+  (dolist (value exec-path)
+    (setenv variable
+            (concat (getenv variable) value ":")))
+  (setenv variable (substring (getenv variable) 0 -1)))
+
+;;; ----------------------------------------------------------------------------
 ;;; Start up default processes
 ;;; ----------------------------------------------------------------------------
 
