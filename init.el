4ed0421dabc8fbb1eea4daedd112aeed63d26409
Tim Visher <tvisher@monetate.com>
2013-06-01 13:11:15 -0400

    Can't define keys in the python-mode-map until python's loaded! EOM

--- init.el
+++ init.el
@@ -295,10 +295,14 @@
   (interactive)
   (python-nav--backward-sexp))
 
-(define-key python-mode-map (kbd "C-M-f") 'python-nav-forward-sexp)
-(define-key python-mode-map (kbd "C-M-b") 'timvisher/python-nav-backward-sexp)
-(define-key python-mode-map (kbd "C-M-d") 'python-nav-up-list)
-(define-key python-mode-map (kbd "C-M-u") 'python-nav-backward-up-list)
+(defun timvisher/map-custom-python-mode-keys ()
+  (define-key python-mode-map (kbd "C-M-f") 'python-nav-forward-sexp)
+  (define-key python-mode-map (kbd "C-M-b") 'timvisher/python-nav-backward-sexp)
+  (define-key python-mode-map (kbd "C-M-d") 'python-nav-up-list)
+  (define-key python-mode-map (kbd "C-M-u") 'python-nav-backward-up-list))
+
+(eval-after-load 'python
+  '(timvisher/map-custom-python-mode-keys))
 
 (defun timvisher/map-custom-paredit-keys ()
   (define-key paredit-mode-map (kbd "C-h") 'paredit-backward-delete)
