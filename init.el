8eb9cc8c245409d03f2f8cb7070ed0e177b5359e
Tim Visher <tim.visher@gmail.com>
2013-06-02 21:57:10 -0400

    First stab at making C-h do the right thing in smex. Doesn't work. Doesn't break. EOM

--- init.el
+++ init.el
@@ -17,6 +17,10 @@
 
 ;;; <f1> should be the only way to access help, because C-h is better bound to backward-delete-char-untabify
 (define-key isearch-mode-map (kbd "C-h") 'isearch-delete-char)
+;;; I would think the following would suffice to have C-h do the right thing in smex but it appears not to.
+(define-key ido-completion-map (kbd "<f1> f") 'smex-describe-function)
+(define-key ido-completion-map (kbd "<f1> w") 'smex-where-is)
+(define-key ido-completion-map (kbd "C-h") 'delete-backward-char)
 
 ;;; You always wants your custom info files to be in the Info-directory-list
 (eval-after-load 'info 
