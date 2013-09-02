c5a3ad37abd73767441f28de7f80ff111bd23892
Tim Visher <tim.visher@gmail.com>
2013-09-02 13:19:21 -0400

    Deleting unused packages? EOM

--- elpa/ido-ubiquitous-1.7/ido-ubiquitous-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ido-ubiquitous" "1.7" "Use ido (nearly) everywhere." (quote nil))
--- elpa/vimgolf-0.9.2/vimgolf-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; vimgolf-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (vimgolf) "vimgolf" "vimgolf.el" (20890 33904 0
-;;;;;;  0))
-;;; Generated autoloads from vimgolf.el
-
-(autoload 'vimgolf "vimgolf" "\
-Open a VimGolf Challenge
-
-\(fn CHALLENGE-ID)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("vimgolf-pkg.el") (20890 33904 515848
-;;;;;;  0))
-
-;;;***
-
-(provide 'vimgolf-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; vimgolf-autoloads.el ends here
