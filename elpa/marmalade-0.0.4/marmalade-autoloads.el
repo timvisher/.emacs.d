f7c8c9de605d586f15a4264f0d65ad5fd4027fad
Tim Visher <tvisher@monetate.com>
2013-10-15 20:15:17 -0400

    Adding clojure packages. EOM

---
+++ elpa/marmalade-0.0.4/marmalade-autoloads.el
@@ -0,0 +1,18 @@
+;;; marmalade-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("marmalade-pkg.el" "marmalade.el") (21085
+;;;;;;  39974 472701 0))
+
+;;;***
+
+(provide 'marmalade-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; marmalade-autoloads.el ends here
---
+++ elpa/pkg-info-0.1/pkg-info-pkg.el
@@ -0,0 +1,1 @@
+(define-package "pkg-info" "0.1" "Information about packages       -*- lexical-binding: t; -*-" (quote ((dash "1.6.0") (s "1.6.0"))))
