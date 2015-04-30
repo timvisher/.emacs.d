b8281b370fd93f19c4721ba7e654ba677c7d2ec3
Tim Visher <tvisher@rjmetrics.com>
2015-04-30 08:44:23 -0400

    Bump rainbow delimiters to 2.1.1

--- elpa/rainbow-delimiters-2.1.1/rainbow-delimiters-autoloads.el
+++ elpa/rainbow-delimiters-2.1.1/rainbow-delimiters-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "rainbow-delimiters" "rainbow-delimiters.el"
-;;;;;;  (21689 12344 646827 66000))
+;;;;;;  (21826 9052 0 0))
 ;;; Generated autoloads from rainbow-delimiters.el
 
 (autoload 'rainbow-delimiters-mode "rainbow-delimiters" "\
