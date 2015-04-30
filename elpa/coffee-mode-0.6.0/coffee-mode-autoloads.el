2f18fb1d2074d1c7b02aa126a1318b428da86d29
Tim Visher <tvisher@rjmetrics.com>
2015-04-30 08:49:03 -0400

    bump coffee-mode to 0.6.0

--- elpa/coffee-mode-0.6.0/coffee-mode-autoloads.el
+++ elpa/coffee-mode-0.6.0/coffee-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (21731 25420
+;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (21826 9053
 ;;;;;;  0 0))
 ;;; Generated autoloads from coffee-mode.el
 
