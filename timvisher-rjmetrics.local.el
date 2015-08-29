f82bcda3f4c9a9bccc6d945c080c9ae17328be88
Tim Visher <tvisher@rjmetrics.com>
2015-08-29 08:02:32 -0400

    require and configure php mode for rjmetrics

--- timvisher-rjmetrics.local.el
+++ timvisher-rjmetrics.local.el
@@ -1,3 +1,9 @@
 (load-theme 'solarized-light)
 
 (require 'mdfind)
+
+(require 'php-mode)
+
+(defun timvisher/indent-tabs-mode () (setq indent-tabs-mode t))
+
+(add-hook 'php-mode-hook 'timvisher/indent-tabs-mode)
