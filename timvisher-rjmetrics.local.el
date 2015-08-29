e9915d2d94a9aceb10df82c12af46b76d78eb140
Tim Visher <tvisher@rjmetrics.com>
2015-08-29 08:06:00 -0400

    Add note about pbcopy and tramp

--- timvisher-rjmetrics.local.el
+++ timvisher-rjmetrics.local.el
@@ -7,3 +7,7 @@
 (defun timvisher/indent-tabs-mode () (setq indent-tabs-mode t))
 
 (add-hook 'php-mode-hook 'timvisher/indent-tabs-mode)
+
+;;; FIXME Main problem is that this breaks on tramp. Should be some
+;;; way to disable it for tramp files only.
+;; (turn-on-pbcopy)
