1034d43f801b25eccf47dbacc262cf46bb1e9031
Tim Visher <tvisher@rjmetrics.com>
2015-08-31 12:24:54 -0400

    Delete git-commit-mode and git-rebase-mode

--- elpa/git-commit-mode-1.0.0/git-commit-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit-mode" "1.0.0" "Major mode for editing git commit messages" 'nil :url "https://github.com/magit/git-modes" :keywords '("convenience" "vc" "git"))
