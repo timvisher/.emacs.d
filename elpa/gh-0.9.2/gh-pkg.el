7ee0da9a71192d272b277dfb1bf3bbed8f8417ef
Tim Visher <tvisher@rjmetrics.com>
2015-10-11 11:48:23 -0400

    magit 2.2.2, magit-gh-pulls 0.5.2

---
+++ elpa/gh-0.9.2/gh-pkg.el
@@ -0,0 +1,7 @@
+(define-package "gh" "0.9.2" "A GitHub library for Emacs"
+  '((eieio "1.4")
+    (pcache "0.3.0")
+    (logito "0.1")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/logito-0.1/logito-pkg.el
@@ -0,0 +1,1 @@
+(define-package "logito" "0.1" "logging library for Emacs" '((eieio "1.3")) :keywords '("lisp" "tool"))
