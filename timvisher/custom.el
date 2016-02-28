f926957d27c8ceb26577d94ce3eac92cdb0c0855
Tim Visher <tvisher@rjmetrics.com>
2016-02-28 11:31:03 -0500

    Correct hl-yellow face

--- timvisher/custom.el
+++ timvisher/custom.el
@@ -218,4 +218,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(hi-yellow ((t (:background "color-226"))))
  '(whitespace-tab ((t (:inherit hl-line :foreground "#e5e5e5")))))
