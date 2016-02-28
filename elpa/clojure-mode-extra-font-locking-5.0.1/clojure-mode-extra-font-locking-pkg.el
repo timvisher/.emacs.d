a5c98a69618227875fbd21da25acce48eafe19ea
Tim Visher <tvisher@rjmetrics.com>
2016-02-28 11:29:30 -0500

    clojure-mode 5.2.0

---
+++ elpa/clojure-mode-extra-font-locking-5.0.1/clojure-mode-extra-font-locking-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clojure-mode-extra-font-locking" "5.0.1" "Extra font-locking for Clojure mode" '((clojure-mode "3.0")) :url "http://github.com/clojure-emacs/clojure-mode" :keywords '("languages" "lisp"))
